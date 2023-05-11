a6dc4c98cd843485b2271d44fda44a3e9fda87f1
Marco Fiorito <marcofiorito1@gmail.com>
2023-05-11 11:03:16 -0500

    primer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimerComponenteComponent
   ],
   imports: [
     BrowserModule
